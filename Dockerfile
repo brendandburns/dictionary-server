8d11df1ab355f752df762d8bdcc236c0a96105ab
Brendan Burns <brendan.d.burns@gmail.com>
2017-02-18 17:19:25 -0800

    initial commit.

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM node:7.5-slim
+
+COPY server.js /server.js
+
+CMD node /server.js
---
+++ server.js
@@ -0,0 +1,85 @@
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var url = require('url');
+
+var dictionary = null;
+
+var dictionaryHandler = (request, response) => {
+    var u = url.parse(request.url);
+
+    if (u.pathname == '/readyz') {
+        if (dictionary) {
+            response.writeHead(200);
+            response.end('OK');
+        } else {
+            response.writeHead(404);
+            response.end('Not Loaded');
+        }
+        return;
+    }
+
+    var key = '';
+    if (u.pathname.length > 0) {
+        key = u.pathname.substr(1).toUpperCase(); 
+    }
+    var def = dictionary[key];
+    if (!def) {
+        response.writeHead(404);
+        response.end(key + ' was not found');
+        return;
+    }
+    response.writeHead(200);
+    response.end(def);
+}
+
+var downloadDictionary = (url, file, callback) => {
+  var stream = fs.createWriteStream(file);
+  var req = https.get(url, function(res) {
+    res.pipe(stream);
+    stream.on('finish', function() {
+      stream.close(callback);
+      console.log('dictionary downloaded');
+    });
+  }).on('error', function(err) {
+    fs.unlink(file);
+    if (callback) cb(err.message);
+  });
+};
+
+var loadDictionary = (file, callback) => {
+    fs.readFile(file, (err, data) => {
+        if (err) {
+            console.log(err);
+            callback(err);
+            return;
+        }
+        dictionary = JSON.parse(data);
+        console.log('dictionary loaded.');
+        callback();
+    })
+};
+
+downloadDictionary('https://raw.githubusercontent.com/adambom/dictionary/master/dictionary.json', 'dictionary.json', (err) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    loadDictionary('dictionary.json', (err) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log('ready to serve');
+    });
+});
+
+const server = http.createServer(dictionaryHandler);
+
+server.listen(8080, (err) => {  
+  if (err) {
+    return console.log('error starting server: ' + err);
+  }
+
+  console.log('server is listening on 8080');
+});
